999962b0c08fa1801e97ac90e5f752644ce8b364
Sergio Angel <chechoap@gmail.com>
2024-11-23 13:54:08 -0500

    Automaton Logic Fully Implemented
    - Still need to work on avanzar function and graphics for each interaction.

--- automata.py
+++ automata.py
@@ -9,24 +9,56 @@ class Automata:
         self.estado_anterior = None
 
     def validar_entrada(self, entrada):
+        """
+        Valida una secuencia de entrada separada por comas.
+        Retorna un diccionario con el resultado.
+        """
+        if isinstance(entrada, str):
+            entrada = entrada.split(",")
+
         for simbolo in entrada:
+            simbolo = simbolo.strip()
             if simbolo not in self.alfabeto:
-                print(f"Error. El símbolo '{simbolo}' no está definido en el alfabeto. Saliendo del juego...")
-                return False
+                return {
+                    "exito": False,
+                    "mensaje": f"Error. El símbolo '{simbolo}' no está definido en el alfabeto.",
+                    "estado_actual": self.estado_actual
+                }
             else:
-                if self.avanzar(simbolo) == False:
-                    return False
-        return True
-    
+                return {
+                    "exito": True,
+                    "mensaje": f"Cadena aceptada.",
+                    "estado_actual": self.estado_actual
+                }
+            
+        return {
+            "exito": True,
+            "mensaje": "Entrada validada correctamente.",
+            "estado_actual": self.estado_actual
+        }
+
     def avanzar(self, simbolo):
+        """
+        Avanza al siguiente estado basado en el símbolo de entrada.
+        Retorna un diccionario con el resultado.
+        """
         if simbolo in self.transiciones[self.estado_actual]:
             self.estado_anterior = self.estado_actual
             self.estado_actual = self.transiciones[self.estado_actual][simbolo]
-            print(f"Estado Anterior: {self.estado_anterior}. Estado Actual: {self.estado_actual}")
-            return True
-        else: 
-            print(f"Error. La entrada '{simbolo}' no es válida para el estado actual '{self.estado_actual}'. Saliendo del juego...")
-            return False
+            return {
+                "exito": True,
+                "mensaje": f"Estado Anterior: {self.estado_anterior}. Estado Actual: {self.estado_actual}",
+                "estado_actual": self.estado_actual
+            }
+        else:
+            return {
+                "exito": False,
+                "mensaje": f"Error. La entrada '{simbolo}' no es válida para el estado actual '{self.estado_actual}'.",
+                "estado_actual": self.estado_actual
+            }
 
-    def es_estado_final(self, estado):
+    def es_estado_final(self):
+        """
+        Verifica si el estado actual es un estado final.
+        """
         return self.estado_actual in self.finales
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from automata import Automata
+from tkinter import ttk
 
 automata_poketower = Automata(
     estados={
@@ -48,35 +49,79 @@ automata_poketower = Automata(
     finales={"q11"}
 )
 
-# Lista de entrada y un índice para rastrear la posición actual
-entrada = ["SIC", "SP", "SH", "SAB", "SO", "EAH", "FJ"]
-indice_actual = 0  # Índice inicial
-
-
-def actualizar_estado():
-    global indice_actual
-    if indice_actual < len(entrada):
-        simbolo = entrada[indice_actual]
-        automata_poketower.avanzar(simbolo)
-        estado_actual_label.config(text=f"Estado actual: {automata_poketower.estado_actual}")
-        indice_actual += 1
-
-        if automata_poketower.es_estado_final(automata_poketower.estado_actual):
-            estado_actual_label.config(text=f"Estado final alcanzado: {automata_poketower.estado_actual}. Juego completado.")
-    else:
-        estado_actual_label.config(text="Fin de la cadena de entrada.")
-        boton_actualizar.config(state="disabled")  # Deshabilitar el botón al finalizar
-
-
-# Configuración de la interfaz gráfica
 root = tk.Tk()
-root.geometry("400x400")
-root.title("Poketower")
+root.geometry("500x200")
+root.title("Poketower - Autómata")
 
-estado_actual_label = tk.Label(root, text=f"Estado actual: {automata_poketower.estado_actual}", font=("Arial", 14))
-estado_actual_label.pack(pady=10)
+def limpiar_root():
+    for widget in root.winfo_children():
+        widget.destroy()
 
-boton_actualizar = tk.Button(root, text="Avanzar", command=actualizar_estado)
-boton_actualizar.pack(pady=10)
+def mostrar_interfaz_inicial(mostrar):
+    if mostrar:
+        global entrada, mensaje_label, boton_validar
+        entrada_label = tk.Label(root, text="Ingresa la secuencia (separada por comas):", font=("Arial", 12))
+        entrada_label.pack(pady=20)
 
+        entrada = tk.Entry(root, font=("Arial", 10), width='40')
+        entrada.pack(pady=10)
+
+        mensaje_label = tk.Label(root, text="", font=("Arial", 10))
+        mensaje_label.pack(pady=10)
+
+        boton_validar = tk.Button(root, text="Validar Entrada", command=validar_entrada_completa, font=("Arial", 12))
+        boton_validar.pack(pady=10)
+    else:
+        limpiar_root()
+
+def validar_entrada_completa():
+    if not entrada.get():
+        mensaje_label.config(text="✖ Debes ingresar una secuencia de entrada.", fg="red")
+        return
+    
+    global entrada_usuario, lista_simbolos
+    entrada_usuario = entrada.get()
+    lista_simbolos = entrada_usuario.split(",")
+    resultado = automata_poketower.validar_entrada(entrada_usuario)
+
+    if resultado["exito"]:
+        mensaje_label.config(text=f"✔ Cadena aceptada. Iniciando autómata...", fg="green")
+        boton_validar.config(state="disabled")
+        entrada.config(state="disabled")
+        root.after(2000, lambda: cambiar_a_interfaz_principal())
+    else:
+        mensaje_label.config(text=f"✖ {resultado['mensaje']}", fg="red")
+
+def avanzar_estado():
+    automata_poketower.avanzar(lista_simbolos[0])
+
+def cambiar_a_interfaz_principal():
+    mostrar_interfaz_inicial(False)
+
+    root.config(width=800, height=600)
+    root.geometry("800x600")
+    tabla = ttk.Treeview(root, columns=("columna1", "columna2", "columna3", "columna4"),show="headings", height=1)
+
+    # Definir las columnas
+    tabla.heading("columna1", text="Cadena")
+    tabla.heading("columna2", text="Símbolo")
+    tabla.heading("columna3", text="Estado Anterior")
+    tabla.heading("columna4", text="Estado Actual")
+
+    # Definir el ancho de las columnas
+    tabla.column("columna1", width=150, anchor="center")
+    tabla.column("columna2", width=150, anchor="center")
+    tabla.column("columna3", width=150, anchor="center")
+    tabla.column("columna4", width=150, anchor="center")
+    tabla.insert("", "end", values=(entrada_usuario, lista_simbolos[0], automata_poketower.estado_anterior, automata_poketower.estado_actual))
+
+    avanzar_boton = tk.Button(root, text="Avanzar", command=avanzar_estado, font=("Arial", 12))
+    avanzar_boton.pack(pady=10)
+
+    tabla.pack(fill=tk.X, expand=False, pady=10)
+
+# Mostrar la interfaz inicial al iniciar la aplicación
+mostrar_interfaz_inicial(True)
+
+# Iniciar el loop principal
 root.mainloop()
