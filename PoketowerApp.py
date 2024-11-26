a87b289ed7087e7ca5a6dc513b558f8a2cbd9f4d
Sergio Angel <chechoap@gmail.com>
2024-11-25 20:53:22 -0500

    Corrected Project Tittle

--- PoketowerApp.py
+++ PoketowerApp.py
@@ -8,7 +8,7 @@ import time
 class PoketowerApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Puchamon Tower")
+        self.root.title("Poketower")
         self.root.geometry("800x600")
 
         self.control_turno = "jugador"
